83f0a40efbc6b576d1f7ab8ed70ab4345d601a49
Caleb Conrad <cmconrad@anderson.edu>
2017-06-26 14:32:49 -0400

    Added color field and stats list

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const arrayList = new Array() 
+
 function updateHeading(ev) {
     ev.preventDefault()
     const f = ev.target
@@ -6,6 +8,28 @@ function updateHeading(ev) {
     heading.innerHTML = nameField
 }
 
+function updateDiv(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const nameField = f.personName.value
+    const div = document.querySelector("div")
+    arrayList.push(nameField)
+    div.innerHTML = nameField
+}
+
+function updateColor(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const colorField = f.color.value
+    const div = document.querySelector("div")
+    arrayList.push(colorField)
+    div.textContent = arrayList
+    document.querySelector("#stats").style.color = colorField
+}
+
 const personForm = document.querySelector("#person-form")
 personForm.addEventListener("submit",updateHeading)
+personForm.addEventListener("submit", updateDiv)
 
+const colorForm = document.querySelector("#color-form")
+colorForm.addEventListener("submit",updateColor)
