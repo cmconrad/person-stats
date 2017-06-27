42c2e71c78e6025ccc9dccb081657f3849a77679
Caleb Conrad <cmconrad@anderson.edu>
2017-06-27 14:26:02 -0400

    Made renderList and renderListItem functions work

--- README.md
+++ README.md
@@ -16,3 +16,24 @@
 ### Super Mega Bonus Credit Hyper Fighting
 
 * Change the appearance of the paragraph (think CSS) based on a value from the form. _e.g._ Turn the text blue if the user types "blue" in the form field.
+
+
+## Day 2 Homework
+
+* Create a new function called `renderColor` that returns a `div` element.
+* Call that function when adding that `div` to `colorItem`.
+
+### Bonus Credit
+
+* Create a new function called `renderListItem`.
+* Use it to create the list items for each stat.
+
+### Super Mega Bonus Credit
+
+* Create a new function called `renderList`.
+* Use it to create the list for each peron's stats.
+* Call `rendereListItem` from `renderList`, not directly from `handleSubmit`.
+
+### Super Mega Bonus Credit Hyper Fighting
+
+* Be strong! Do not resort to the use of `innerHTML`. Keep using `document.createElement`.
--- index.js
+++ index.js
@@ -2,18 +2,46 @@ function handleSubmit(ev){
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const age = f.age.value
     const color = f.color.value
+    const age = f.age.value
     const stats = document.querySelector("#stats")
 
-    stats.innerHTML += `<p>${name}, age: ${age}</p>`
+    renderColor(color)
+
+    //stats.innerHTML += `<ul>${name}, age: ${age}</ul>`
     
+    
+    //list.textContent = `${name}, age ${age} ${color}`
+
+    stats.appendChild(renderList("Name",name,"Age",age,"Color",color))
     // const p = document.createElement('p')
     // p.textContext = `${name}, age ${age}, color ${color}`
     // stats.appendChild(p)
 
-    stats.style.color = color
+    //stats.style.color = color
+}
 
+function renderColor(color){
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6 rem'
+    colorDiv.style.height = '3 rem'
+    //colorItem.appendChild(colorDiv)
+    return colorDiv
+}
+
+function renderListItem(name, value){
+    var item = document.createElement('li')
+    item.textContent = name+": "+value 
+    return item
+}
+
+function renderList(n1,v1,n2,v2,n3,v3){
+    const list = document.createElement('ul')
+    list.appendChild(renderListItem(n1,v1))
+    list.appendChild(renderListItem(n2,v2))
+    list.appendChild(renderListItem(n3,v3))
+    return list
 }
 
 const personForm = document.querySelector("#person-form")
